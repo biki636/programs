16489edb0b42b60a3760b7fdba4c12ff30df018b
Soumitri Pattnaik <soumitri@Soumitris-MacBook-Air.local>
2017-12-23 09:11:09 +0530

    Removed unnecessaary sum arrays

--- Array/MaxSubArray.java
+++ Array/MaxSubArray.java
@@ -5,14 +5,12 @@ class MaxSubArray {
     public static void main(String[] args) {
         
         int[] arr = {1, -2, 9, -5, -1, 4, -2, 8, -1, 0, 5, -1}; // Input array
-        int[] sumArr = new int[arr.length]; // Array to store sum from left to right
         int max = Integer.MIN_VALUE;
         int endIndex = -1;
         int sum = 0;
 
         for(int i = 0 ; i < arr.length ; i ++) { // Find the end index
             sum += arr[i];
-            sumArr[i] = sum;
             if(sum >= max) {
                 max = sum;
                 endIndex = i; 
@@ -22,16 +20,15 @@ class MaxSubArray {
         int rSum = 0;
         int rMax = Integer.MIN_VALUE;
         int startIndex = -1;
-        int[] rSumArr = new int[endIndex + 1]; // Array to store sum from right to left starting from the endIndex
 
         for(int i = endIndex ; i >= 0 ; i --) { // Find the start index
             rSum += arr[i];
-            rSumArr[i] = rSum;
             if(rSum >= rMax) {
                 rMax = rSum;
                 startIndex = i;
             }
         }
+        
         System.out.println("Start index = " + startIndex + " End index = " + endIndex);
     }
 }
