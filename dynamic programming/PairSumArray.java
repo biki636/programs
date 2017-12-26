ca66c48fdc5d245d683e44b8b0a0ae221596cf35
Soumitri Pattnaik <soumitri@Soumitris-MacBook-Air.local>
2017-12-26 18:55:30 +0530

    Find a pair in an array which adds up to a given sum

---
+++ dynamic programming/PairSumArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+// Given an int array and a number, 
+// write a function that takes these two in and returns a pair which adds up to the given number
+// Example : array = {1, 3, 4, 5, 2, 1} | sum = 6 --> O/P : 5, 1 or 4, 2
+
+class PairSumArray {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 4, 5, 2, 1};
+        int sum = 6;
+
+        // First sort the array
+        Arrays.sort(array);
+
+        int i = 0; // First index
+        int j = array.length - 1; // Last index
+        while(i < j) {
+
+            // Check if the first + last is equals or greater or smaller than the sum
+            int tempSum = array[i] + array[j];
+            if(tempSum == sum) {
+                System.out.println("[" + array[i] + ", " + array[j] + "]");
+                return;
+            } else if(tempSum < sum) {
+                i ++;
+            } else {
+                j --;
+            }
+        }
+
+        System.out.println("No match found");
+    }
+}
+ 
