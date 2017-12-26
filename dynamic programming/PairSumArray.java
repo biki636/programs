23a24586645d3bf062cc928fd5789aeced0959d4
biki636 <soumitri@Soumitris-MacBook-Air.local>
2017-12-26 19:12:58 +0530

    Moved comments up

--- dynamic programming/PairSumArray.java
+++ dynamic programming/PairSumArray.java
@@ -1,8 +1,8 @@
-import java.util.Arrays;
-
 // Given an int array and a number, 
 // write a function that takes these two in and returns a pair which adds up to the given number
 // Example : array = {1, 3, 4, 5, 2, 1} | sum = 6 --> O/P : 5, 1 or 4, 2
+
+import java.util.Arrays;
 
 class PairSumArray {
     public static void main(String[] args) {
