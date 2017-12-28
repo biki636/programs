c3a44617f3f09ea020dbc25e0f4e406a0d571be7
Soumitri Pattnaik <biki636@gmail.com>
2017-12-29 00:57:48 +0530

    Added coment for ThreadJoin.java

--- dynamic programming/ThreadJoin.java
+++ dynamic programming/ThreadJoin.java
@@ -1,3 +1,6 @@
+// Suppose one thread is printing 1 3  5 7 9
+// Another thread is printing 2 4 6 8 10
+// Run these two threds such that the output will be 1 2 3 4 5 6 7 8 9 10
 class ThreadJoin {
     public static void main(String[] args) {
         SimpleThread1 t1 = new SimpleThread1();
