29a1af7196ea0ffb64b8df6a55771eb323086360
Soumitri Pattnaik <biki636@gmail.com>
2017-12-29 00:59:06 +0530

    Spelling correction

--- dynamic programming/ThreadJoin.java
+++ dynamic programming/ThreadJoin.java
@@ -1,4 +1,4 @@
-// Suppose one thread is printing 1 3  5 7 9
+// Suppose one thread is printing 1 3 5 7 9
 // Another thread is printing 2 4 6 8 10
 // Run these two threds such that the output will be 1 2 3 4 5 6 7 8 9 10
 class ThreadJoin {
